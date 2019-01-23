a13d88605b258ccb5667afeb4d0fe908ae745e8c
Yura Sultonov <yura_sultonov@mail.ru>
2019-01-24 02:56:14 +0500

    added certificates, moved skill images to skills folder

--- js/education.js
+++ js/education.js
@@ -1,12 +1,192 @@
 var awards = [
         {
-            "title": "Honorable mention of ACM ICPC Regional contest 2014",
-            "by": "Tashkent, 7 december, 2014",
-            "url": "/img/certificates/neerc_regional_2014.jpg"
-        }
+            "title": "Diploma of the winner of the competition “Yurt Kelajagi 2017“ at the regional stage",
+            "by": "Khorezm region branch of youth movement “Kamolot”, may 8, 2017",
+            "url": "/img/certificates/2017.05.08.jpg"
+        },
+        {
+            "title": "Certificate of achievement for first place at the contest “Software contest for learning and enhancing foreign languages“",
+            "by": "Tashkent university of information technologies, april 19-20, 2017",
+            "url": "/img/certificates/2017.04.20.jpg"
+        },
+        {
+            "title": "Certificate for active participation in the programming competition among TUIT, its branches and INHA University.",
+            "by": "Tashkent university of information technologies, february 26, 2017",
+            "url": "/img/certificates/2017.02.26.jpg"
+        },
+        {
+            "title": "Certificate of appreciation by Urgench branch of TUIT for obtaining scholarships Beruniy",
+            "by": "Urgench branch of TUIT, december 2, 2016",
+            "url": "/img/certificates/2016.12.02.jpg"
+        },
+        {
+            "title": "Certificate of achievement for eighth place at ACM ICPC subregional contest",
+            "by": "NEERC ICPC, november 13, 2016",
+            "url": "/img/certificates/2016.11.13.jpg"
+        },
+        {
+            "title": "Certificate for the Beruni scholarship",
+            "by": "Ministry of higher and secondary specialized education of republic Uzbekistan, november 4, 2016",
+            "url": "/img/certificates/2016.11.04.jpg"
+        },
+        {
+            "title": "Certificate for participation in the Cyber security challenger Uzbekistan 2016",
+            "by": "youth movement “Kamolot”, september 21, 2016",
+            "url": "/img/certificates/2016.09.21.jpg"
+        },
+        {
+            "title": "Certificate of completion Samsung VR Course",
+            "by": "Samsung Employee Volunteer Program, september 9, 2016",
+            "url": "/img/certificates/2016.09.09.jpg"
+        },
+        {
+            "title": "Certificate for participation in the competition Yurt Kelajagi 2016 at the republican stage",
+            "by": "Youth movement “Kamolot”, august 18, 2016",
+            "url": "/img/certificates/2016.08.18.jpg"
+        },
+        {
+            "title": "Diploma of the first degree for the first place at the Republican Olympiad in computer graphics.",
+            "by": "Ministry of higher and secondary specialized education of republic Uzbekistan, june 20, 2016",
+            "url": "/img/certificates/2016.06.20.jpg"
+        },
+        {
+            "title": "Certificate of achievement for first place at the Republican Olympiad in computer graphics.",
+            "by": "Tashkent university of information technologies, may 12, 2016",
+            "url": "/img/certificates/2016.05.12.jpg"
+        },
+        {
+            "title": "Diploma of the winner of the competition “Yurt Kelajagi 2016“ at the regional stage",
+            "by": "Khorezm region branch of youth movement “Kamolot”, may 5, 2016",
+            "url": "/img/certificates/2016.05.05.jpg"
+        },
+        {
+            "title": "Certificate of achievement for third place at the contest “Software contest for learning and enhancing foreign languages“",
+            "by": "Tashkent university of information technologies, april 14, 2016",
+            "url": "/img/certificates/2016.04.14.jpg"
+        },
+        {
+            "title": "Certificate for active participation in the infographic exhibition under the slogan “We are the youth against extremism and terrorism“",
+            "by": "Urgench branch of TUIT, march 30, 2016",
+            "url": "/img/certificates/2016.03.30.jpg"
+        },
+        {
+            "title": "Certificate of achievement confirms english language skills",
+            "by": "Well studies education, march 25, 2016",
+            "url": "/img/certificates/2016.03.25.jpg"
+        },
+        {
+            "title": "Certificate of honor for voluntary public mutual aid",
+            "by": "The charitable public fund of Uzbekistan “Mahalla“, march 12, 2016",
+            "url": "/img/certificates/2016.03.12.jpg"
+        },
+        {
+            "title": "Certificate confirming publication on the journal “Scientist of the 21st Century“",
+            "by": "“КОЛЛОКВИУМ“ LLC, march 4, 2016",
+            "url": "/img/certificates/2016.03.04.jpg"
+        },
+        {
+            "title": "Certificate for active participation in the project “Secure internet“",
+            "by": "The Liberal Democratic Party of Uzbekistan, january 15, 2016",
+            "url": "/img/certificates/2016.01.15.jpg"
+        },
+        {
+            "title": "Certificate of Honor for excellent study",
+            "by": "Urgench branch of TUIT, december 22, 2015",
+            "url": "/img/certificates/2015.12.22.jpg"
+        },
+        {
+            "title": "Certificate of achievement for participating at ACM ICPC regional contest",
+            "by": "ACM programming contest, december 5, 2015",
+            "url": "/img/certificates/2015.12.05.jpg"
+        },
+        {
+            "title": "Certificate of achievement for participating at internet-conference “Trends and prospects of the development of science and education in the context of globalization“",
+            "by": "Pereyaslav-Khmelnitsky State Pedagogical University named after Gregory Skovoroda, december 4, 2015",
+            "url": "/img/certificates/2015.12.04.jpg"
+        },
+        {
+            "title": "Certificate of achievement for participating at quarter-finals ACM ICPC contest",
+            "by": "ACM programming contest, october 23, 2015",
+            "url": "/img/certificates/2015.10.23.jpg"
+        },
+        {
+            "title": "Diploma of the third degree for the third place at the Republican Olympiad in informatics.",
+            "by": "Ministry of higher and secondary specialized education of republic Uzbekistan, june 8, 2015",
+            "url": "/img/certificates/2015.06.08.jpg"
+        },
+        {
+            "title": "Certificate of achievement for third place at the Republican Olympiad in informatics.",
+            "by": "Tashkent university of information technologies, may 14, 2015",
+            "url": "/img/certificates/2015.05.14.jpg"
+        },
+        {
+            "title": "Certificate of achievement for first place in the programming competition among TUIT and its branches.",
+            "by": "Tashkent university of information technologies, april 28, 2015",
+            "url": "/img/certificates/2015.04.28.jpg"
+        },
+        {
+            "title": "Certificate of appreciation to my parents",
+            "by": "Urgench branch of TUIT, april 3, 2015",
+            "url": "/img/certificates/2015.04.03.jpg"
+        },
+        {
+            "title": "Diploma of the first degree for first place in the first stage of programming competition among TUIT and its branches.",
+            "by": "Urgench branch of TUIT, april 2, 2015",
+            "url": "/img/certificates/2015.04.02.jpg"
+        },
+        {
+            "title": "Certificate of achievement for participating at ACM ICPC regional contest",
+            "by": "ACM programming contest, december 7, 2014",
+            "url": "/img/certificates/2014.12.07.jpg"
+        },
+        {
+            "title": "Certificate of achievement for participating at quarter-finals ACM ICPC contest",
+            "by": "ACM programming contest, november 8, 2014",
+            "url": "/img/certificates/2014.11.08.jpg"
+        },
+        {
+            "title": "Certificate of achievement for participating at quarter-finals ACM ICPC contest",
+            "by": "ACM programming contest, november 8, 2014",
+            "url": "/img/certificates/2014.11.08.jpg"
+        },
+    {
+        "title": "Certificate of Honor for excellent study",
+        "by": "Urgench branch of TUIT, september 2, 2014",
+        "url": "/img/certificates/2014.09.02.jpg"
+    },
+    {
+        "title": "Certificate of Honor for excellent study",
+        "by": "Urgench branch of TUIT, september 2, 2014",
+        "url": "/img/certificates/2014.09.02.jpg"
+    },
+    {
+        "title": "Diploma of the winner of the competition “Yurt Kelajagi 2014“ at the republic stage",
+        "by": "Youth movement “Kamolot”, august 8, 2014",
+        "url": "/img/certificates/2014.08.08.jpg"
+    },
+    {
+        "title": "Diploma of the winner of the competition “Yurt Kelajagi 2014“ at the regional stage",
+        "by": "Khorezm branch of youth movement “Kamolot”, july 7, 2014",
+        "url": "/img/certificates/2014.07.07.jpg"
+    },
+    {
+        "title": "Diploma of the winner of the competition “Yurt Kelajagi 2014“ at the subregional stage",
+        "by": "Urgench branch of youth movement “Kamolot”, may 5, 2014",
+        "url": "/img/certificates/2014.05.05.jpg"
+    },
+    {
+        "title": "Diploma of the first degree for the first place at the Olympiad in history.",
+        "by": "Yangibazar branch of “Ministry of higer and secondary specialized education of the republic of Uzbekistan“, april 25, 2008",
+        "url": "/img/certificates/2008.jpg"
+    },
+    {
+        "title": "Diploma of the first degree for the first place at the Olympiad in mathematics.",
+        "by": "Yangibazar branch of “Ministry of higer and secondary specialized education of the republic of Uzbekistan“, march 28, 2004",
+        "url": "/img/certificates/2004.jpg"
+    },
     ]
 ;
-$(document).ready(function() {
+$(document).ready(function () {
     $('.popup-gallery').magnificPopup({
         delegate: 'a',
         type: 'image',
@@ -15,12 +195,12 @@ $(document).ready(function() {
         gallery: {
             enabled: true,
             navigateByImgClick: true,
-            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
         },
         image: {
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded...',
-            titleSrc: function(item) {
-                return item.el.attr('title') + '<small>'+item.el.data('by')+'</small>';
+            titleSrc: function (item) {
+                return item.el.attr('title') + '<small>' + item.el.data('by') + '</small>';
             }
         }
     });
@@ -32,11 +212,11 @@ $(document).ready(function() {
             div.innerHTML += elem;
             elem = "<div class='row'>";
         }
-        elem += "<div class=\"col-md-3 sertificate\">";
-        elem += "<a href=\""+awards[i].url+"\" title=\"" + awards[i].title + "\"";
+        elem += "<div class=\"col-md-3 certificate\">";
+        elem += "<a href=\"" + awards[i].url + "\" title=\"" + awards[i].title + "\"";
         elem += "data-by=\"" + awards[i].by + "\">";
-        elem += "<img class=\"sertificate-item\" src=\""+awards[i].url+"\">";
-        elem += "<p class='certificate-text'>"+awards[i].title+"</p>";
+        elem += "<img class=\"certificate-item\" src=\"" + awards[i].url + "\">";
+        elem += "<p class='certificate-text'>" + awards[i].title + "</p>";
         elem += "</a></div>";
         if (i === awards.length - 1)
             div.innerHTML += elem + "</div>";
